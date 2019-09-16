a0bdb09e79b6f5a3ec5fa48ea38641a51ce0b584
michael <michaeleburrell@gmail.com>
2019-09-16 11:26:40 -0700

    adds figaro gem

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class Api::RestaurantsController < ApplicationController
 
   def index
-    @restaurants = HTTP.headers("user-key" => "d1abcedc517fd4f375d07c93fbebab73").get("https://developers.zomato.com/api/v2.1/cities?q=new%20york").parse["location_suggestions"]
+    @restaurants = HTTP.headers("user-key" => "#{ENV['API_KEY']}").get("https://developers.zomato.com/api/v2.1/cities?q=new%20york").parse["location_suggestions"]
 
     render "index.json.jb"
   end
